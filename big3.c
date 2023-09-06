5628b1af55f8c539dde671985fd57ecff63b0139
deepak8971 <143609773+deepak8971@users.noreply.github.com>
2023-09-06 17:01:44 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -4,7 +4,7 @@ biggest() {
 
   double n1, n2, n3;
 
-  printf("Enter three different numbers\n: ");
+  printf("Enter three numbers\n: ");
   scanf("%lf %lf %lf", &n1, &n2, &n3);
 
   // if n1 is greater than both n2 and n3, n1 is the largest
